20fab372005f894d04986271d8a799c70989f5fd
Arjzona <151389775+Arjzona@users.noreply.github.com>
2025-03-06 13:22:15 +0300

    Delete t1.php

--- t1.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-function getDayOfYear($dateString) {
-    $date = new DateTime($dateString);
-    $start = new DateTime($date->format('Y-01-01'));
-    $diff = $date->diff($start);
-    return $diff->days + 1;
-}
-?>
