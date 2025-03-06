440898b9e999884cd8b801f21c84d79e8a55d6fd
Arjzona <151389775+Arjzona@users.noreply.github.com>
2025-03-06 13:19:29 +0300

    Add files via upload

---
+++ лр22/script5.php
@@ -0,0 +1,13 @@
+﻿<?php
+echo "Имя текущего скрипта: " . $_SERVER['SCRIPT_NAME'] . "<br>";
+
+$username = "Валерия";
+
+echo "Привет, $username! <br>";
+
+echo "Версия PHP: " . PHP_VERSION . "<br>";
+
+$number = 69;
+
+echo "Число: $number <br>";
+?>
