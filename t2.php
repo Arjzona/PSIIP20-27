927851dd0872478ec4831ecd3176efac4ea20d4d
Arjzona <151389775+Arjzona@users.noreply.github.com>
2025-03-06 13:14:24 +0300

    Add files via upload

---
+++ t2.php
@@ -0,0 +1,8 @@
+﻿<?php
+$fullName = "Алексейчук Валерия";
+$timesToShow = 6;
+
+for ($i = 0; $i < $timesToShow; $i++) {
+    echo $fullName . "<br>";
+}
+?>
---
+++ t5.php
@@ -0,0 +1,14 @@
+﻿<?php
+function calculateY($x) {
+    try {
+        if (!is_numeric($x)) throw new Exception("Входное значение должно быть числом");
+        return pow($x - 2, 2);
+    } catch (Exception $e) {
+        return "Ошибка: " . $e->getMessage();
+    }
+}
+
+$x = 5; 
+$y = calculateY($x);
+echo "Результат расчета y = (x-2)^2 для x={$x}: {$y}";
+?>
